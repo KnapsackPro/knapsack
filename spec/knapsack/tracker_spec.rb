b9eb8747c025caaec2a6ad0153a38d9f7b64a838
ArturT <arturtrzop@gmail.com>
2014-07-04 22:22:08 +0200

    Add spec for time track execution

--- spec/knapsack/tracker_spec.rb
+++ spec/knapsack/tracker_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
+shared_examples 'default trakcer attributes' do
+  it { expect(tracker.global_time).to eql 0 }
+  it { expect(tracker.files).to eql({}) }
+end
+
 describe Knapsack::Tracker do
   let(:tracker) { described_class.send(:new) }
 
-  describe 'attributes' do
-    it { expect(tracker.global_time).to eql 0 }
-    it { expect(tracker.files).to eql({}) }
-  end
+  it_behaves_like 'default trakcer attributes'
 
   describe '#generate_report?' do
     subject { tracker.generate_report? }
@@ -48,6 +50,38 @@ describe Knapsack::Tracker do
           time_offset_warning: 30,
         })
       end
+    end
+  end
+
+  describe 'track time execution' do
+    let(:now) { Time.now }
+    let(:spec_paths) { ['a_spec.rb', 'b_spec.rb'] }
+
+    before do
+      spec_paths.each_with_index do |spec_path, index|
+        Timecop.freeze(now) do
+          tracker.spec_path = spec_path
+          tracker.start_timer
+        end
+
+        seconds = index + 1
+        Timecop.freeze(now+seconds) do
+          tracker.stop_timer
+        end
+      end
+    end
+
+    it { expect(tracker.global_time).to eql 3.0 }
+    it do
+      expect(tracker.files).to eql({
+        'a_spec.rb' => 1.0,
+        'b_spec.rb' => 2.0,
+      })
+    end
+
+    context '#reset!' do
+      before { tracker.reset! }
+      it_behaves_like 'default trakcer attributes'
     end
   end
 end
