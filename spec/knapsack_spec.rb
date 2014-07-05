f1fbc1557649f3ea12e53ee078fe9e66a807ec5c
ArturT <arturtrzop@gmail.com>
2014-07-05 10:20:24 +0200

    Update knapsack spec

--- spec/knapsack_spec.rb
+++ spec/knapsack_spec.rb
@@ -7,4 +7,11 @@ describe Knapsack do
     it { should be_a Knapsack::Tracker }
     it { expect(subject.object_id).to eql described_class.tracker.object_id }
   end
+
+  describe '.report' do
+    subject { described_class.report }
+
+    it { should be_a Knapsack::Report }
+    it { expect(subject.object_id).to eql described_class.report.object_id }
+  end
 end
