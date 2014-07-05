df21810315a8313f3609d48affee21a58fadeb77
ArturT <arturtrzop@gmail.com>
2014-07-05 09:28:18 +0200

    Allow to pass report path in Report class

--- lib/knapsack/report.rb
+++ lib/knapsack/report.rb
@@ -3,14 +3,14 @@ module Knapsack
     REPORT_PATH = 'knapsack_report.json'
 
     class << self
-      def save
-        File.open(REPORT_PATH, 'w+') do |f|
+      def save(report_path=nil)
+        File.open(report_path || REPORT_PATH, 'w+') do |f|
           f.write(Presenter.report_json)
         end
       end
 
-      def open
-        report = File.read(REPORT_PATH)
+      def open(report_path=nil)
+        report = File.read(report_path || REPORT_PATH)
         JSON.parse(report)
       rescue Errno::ENOENT
         raise "Knapsack report file doesn't exist. Please generate report first!"
