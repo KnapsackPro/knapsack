28cf25c67f77c2ba14610ddb0888508fe651591d
ArturT <arturtrzop@gmail.com>
2014-07-05 23:55:20 +0200

    Add adapter base spec

--- lib/knapsack/adapters/base.rb
+++ lib/knapsack/adapters/base.rb
@@ -2,23 +2,29 @@ module Knapsack
   module Adapters
     class Base
       def self.bind
-        new
+        adapter = new
+        adapter.bind
+        adapter
       end
 
-      protected
-
-      def initialize
-        if Knapsack.tracker.generate_report?
+      def bind
+        if tracker.generate_report?
           puts 'Knapsack report generator started!'
           bind_time_tracker
           bind_report_generator
-        elsif Knapsack.tracker.config[:enable_time_offset_warning]
+        elsif tracker.config[:enable_time_offset_warning]
           puts 'Knapsack time offset warning enabled!'
           bind_time_tracker
           # TODO
         else
           puts 'Knapsack is off!'
         end
+      end
+
+      protected
+
+      def tracker
+        Knapsack.tracker
       end
 
       def bind_time_tracker
---
+++ spec/knapsack/adapters/base_spec.rb
@@ -0,0 +1,62 @@
+describe Knapsack::Adapters::Base do
+  let(:adapter) { instance_double(described_class) }
+
+  describe '.bind' do
+    subject { described_class.bind }
+
+    before do
+      expect(described_class).to receive(:new).and_return(adapter)
+      expect(adapter).to receive(:bind)
+    end
+
+    it { should eql adapter }
+  end
+
+  describe '#bind' do
+    let(:tracker) { instance_double(Knapsack::Tracker) }
+
+    subject { described_class.new }
+
+    before do
+      allow(subject).to receive(:tracker).and_return(tracker)
+    end
+
+    context 'when generate report' do
+      before do
+        expect(tracker).to receive(:generate_report?).and_return(true)
+      end
+
+      it do
+        expect(subject).to receive(:bind_time_tracker)
+        expect(subject).to receive(:bind_report_generator)
+        subject.bind
+      end
+    end
+
+    context 'when enable time offset warning' do
+      before do
+        expect(tracker).to receive(:generate_report?).and_return(false)
+        expect(tracker).to receive(:config).and_return({ enable_time_offset_warning: true })
+      end
+
+      it do
+        expect(subject).to receive(:bind_time_tracker)
+        expect(subject).not_to receive(:bind_report_generator)
+        subject.bind
+      end
+    end
+
+    context 'when adapter is off' do
+      before do
+        expect(tracker).to receive(:generate_report?).and_return(false)
+        expect(tracker).to receive(:config).and_return({ enable_time_offset_warning: false })
+      end
+
+      it do
+        expect(subject).not_to receive(:bind_time_tracker)
+        expect(subject).not_to receive(:bind_report_generator)
+        subject.bind
+      end
+    end
+  end
+end
