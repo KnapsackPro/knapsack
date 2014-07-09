e2e5157478ab1cd90c9baf62461bcbc180b5c6fb
ArturT <artur.trzop@lunarlogic.io>
2014-07-09 13:45:20 +0200

    Add leftover spec examples not included in knapsack report

---
+++ spec_examples/leftover/1_spec.rb
@@ -0,0 +1,4 @@
+# this file should not be included in knapsack_report.json
+describe 'Leftover Fast 1' do
+  it {}
+end
---
+++ spec_examples/leftover/a_spec.rb
@@ -0,0 +1,6 @@
+# this file should not be included in knapsack_report.json
+describe 'Leftover Slow A' do
+  it { sleep 1 }
+  it { sleep 0.1 }
+  it { sleep 0.5 }
+end
