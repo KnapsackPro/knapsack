7e1af3e1f03cf5e5985ca7b051112ebc8e540fba
ArturT <artur.trzop@lunarlogic.io>
2014-07-10 19:39:19 +0200

    [wip] allocator spec

--- spec/knapsack/allocator_spec.rb
+++ spec/knapsack/allocator_spec.rb
@@ -1,3 +1,22 @@
 describe Knapsack::Allocator do
-  # TODO
+  let(:args) { {} }
+  let(:report_distributor) { instance_double(Knapsack::Distributors::ReportDistributor) }
+  let(:leftover_distributor) { instance_double(Knapsack::Distributors::LeftoverDistributor) }
+  let(:specs) { double }
+  let(:allocator) { described_class.new(args) }
+
+  before do
+    expect(Knapsack::Distributors::ReportDistributor).to receive(:new).with(args).and_return(report_distributor)
+    expect(Knapsack::Distributors::LeftoverDistributor).to receive(:new).with(args).and_return(leftover_distributor)
+  end
+
+  describe '#report_node_specs' do
+    subject { allocator.report_node_specs }
+
+    before do
+      expect(report_distributor).to receive(:specs_for_current_node).and_return(specs)
+    end
+
+    it { should eql specs }
+  end
 end
