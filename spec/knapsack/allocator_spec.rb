68b53d85f05fba79501a951bf85e766117eb4ec6
ArturT <arturtrzop@gmail.com>
2014-07-10 23:46:44 +0200

    Remove duplicated code in allocator spec

--- spec/knapsack/allocator_spec.rb
+++ spec/knapsack/allocator_spec.rb
@@ -17,47 +17,27 @@ describe Knapsack::Allocator do
   before do
     expect(Knapsack::Distributors::ReportDistributor).to receive(:new).with(args).and_return(report_distributor)
     expect(Knapsack::Distributors::LeftoverDistributor).to receive(:new).with(args).and_return(leftover_distributor)
+    allow(report_distributor).to receive(:specs_for_current_node).and_return(report_specs)
+    allow(leftover_distributor).to receive(:specs_for_current_node).and_return(leftover_specs)
   end
 
   describe '#report_node_specs' do
     subject { allocator.report_node_specs }
-
-    before do
-      expect(report_distributor).to receive(:specs_for_current_node).and_return(report_specs)
-    end
-
     it { should eql report_specs }
   end
 
   describe '#leftover_node_specs' do
     subject { allocator.leftover_node_specs }
-
-    before do
-      expect(leftover_distributor).to receive(:specs_for_current_node).and_return(leftover_specs)
-    end
-
     it { should eql leftover_specs }
   end
 
   describe '#node_specs' do
     subject { allocator.node_specs }
-
-    before do
-      expect(report_distributor).to receive(:specs_for_current_node).and_return(report_specs)
-      expect(leftover_distributor).to receive(:specs_for_current_node).and_return(leftover_specs)
-    end
-
     it { should eql node_specs }
   end
 
   describe '#stringify_node_specs' do
     subject { allocator.stringify_node_specs }
-
-    before do
-      expect(report_distributor).to receive(:specs_for_current_node).and_return(report_specs)
-      expect(leftover_distributor).to receive(:specs_for_current_node).and_return(leftover_specs)
-    end
-
     it { should eql node_specs.join(' ') }
   end
 end
