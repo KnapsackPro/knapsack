92832bf22c46de8eb32d2c5af1ffe523393b0187
Alfredo Lopez QA <alfredo.lopez@kantox.com>
2014-10-02 09:55:20 +0200

    Pull request cleaning.

--- lib/knapsack/adapters/rspec_adapter.rb
+++ lib/knapsack/adapters/rspec_adapter.rb
@@ -6,7 +6,7 @@ module Knapsack
           config.before(:each) do
             current_example_group = ::RSpec.respond_to?(:current_example) ?
                 ::RSpec.current_example.metadata[:example_group] : example.metadata
-            Knapsack.tracker.spec_path = RspecAdapter.spec_path current_example_group
+            Knapsack.tracker.spec_path = RspecAdapter.spec_path(current_example_group)
             Knapsack.tracker.start_timer
           end
 
