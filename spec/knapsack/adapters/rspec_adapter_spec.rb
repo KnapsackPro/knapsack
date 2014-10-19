92bd330bbfce4d1a9beafaf02eb4dbecb5963a7b
ArturT <arturtrzop@gmail.com>
2014-10-19 20:26:56 +0200

    Update expectation order in rspec adapter spec

--- spec/knapsack/adapters/rspec_adapter_spec.rb
+++ spec/knapsack/adapters/rspec_adapter_spec.rb
@@ -24,21 +24,21 @@ describe Knapsack::Adapters::RspecAdapter do
       end
 
       it do
+        expect(config).to receive(:before).with(:each).and_yield
+        expect(config).to receive(:after).with(:each).and_yield
+        expect(config).to receive(:after).with(:suite).and_yield
+        expect(::RSpec).to receive(:configure).and_yield(config)
+
         expect(::RSpec).to receive(:current_example).twice.and_return(current_example)
         expect(described_class).to receive(:spec_path).with(example_group).and_return(spec_path)
 
         allow(Knapsack).to receive(:tracker).and_return(tracker)
         expect(tracker).to receive(:spec_path=).with(spec_path)
         expect(tracker).to receive(:start_timer)
+
         expect(tracker).to receive(:stop_timer)
 
         expect(Knapsack::Presenter).to receive(:global_time).and_return(global_time)
-
-        expect(config).to receive(:before).with(:each).and_yield
-        expect(config).to receive(:after).with(:each).and_yield
-        expect(config).to receive(:after).with(:suite).and_yield
-        expect(::RSpec).to receive(:configure).and_yield(config)
-
         expect(logger).to receive(:info).with(global_time)
 
         subject.bind_time_tracker
@@ -50,14 +50,13 @@ describe Knapsack::Adapters::RspecAdapter do
       let(:report_details) { 'Report details' }
 
       it do
+        expect(config).to receive(:after).with(:suite).and_yield
+        expect(::RSpec).to receive(:configure).and_yield(config)
+
         expect(Knapsack).to receive(:report).and_return(report)
         expect(report).to receive(:save)
 
         expect(Knapsack::Presenter).to receive(:report_details).and_return(report_details)
-
-        expect(config).to receive(:after).with(:suite).and_yield
-        expect(::RSpec).to receive(:configure).and_yield(config)
-
         expect(logger).to receive(:info).with(report_details)
 
         subject.bind_report_generator
@@ -68,11 +67,10 @@ describe Knapsack::Adapters::RspecAdapter do
       let(:time_offset_warning) { 'Time offset warning' }
 
       it do
-        expect(Knapsack::Presenter).to receive(:time_offset_warning).and_return(time_offset_warning)
-
         expect(config).to receive(:after).with(:suite).and_yield
         expect(::RSpec).to receive(:configure).and_yield(config)
 
+        expect(Knapsack::Presenter).to receive(:time_offset_warning).and_return(time_offset_warning)
         expect(logger).to receive(:warn).with(time_offset_warning)
 
         subject.bind_time_offset_warning
