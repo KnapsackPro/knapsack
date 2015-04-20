33700258a1405be0208a37a985c970fb7261bdab
ArturT <arturtrzop@gmail.com>
2015-04-20 22:31:28 +0200

    Fix spec for task loader

--- spec/knapsack/task_loader_spec.rb
+++ spec/knapsack/task_loader_spec.rb
@@ -2,10 +2,12 @@ describe Knapsack::TaskLoader do
   describe '#load_tasks' do
     let(:rspec_rake_task_path) { "#{Knapsack.root}/lib/tasks/knapsack_rspec.rake" }
     let(:cucumber_rake_task_path) { "#{Knapsack.root}/lib/tasks/knapsack_cucumber.rake" }
+    let(:minitest_rake_task_path) { "#{Knapsack.root}/lib/tasks/knapsack_minitest.rake" }
 
     it do
       expect(subject).to receive(:import).with(rspec_rake_task_path)
       expect(subject).to receive(:import).with(cucumber_rake_task_path)
+      expect(subject).to receive(:import).with(minitest_rake_task_path)
       subject.load_tasks
     end
   end
