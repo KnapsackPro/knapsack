2886020a7314dccfeea948f66bafba4e621c533b
ArturT <arturtrzop@gmail.com>
2015-05-01 22:36:21 +0200

    Get rid of Kernel at exit in minitest adapter

--- lib/knapsack/adapters/minitest_adapter.rb
+++ lib/knapsack/adapters/minitest_adapter.rb
@@ -15,7 +15,6 @@ module Knapsack
       module BindTimeTrackerMinitestPlugin
         def before_setup
           super
-
           Knapsack.tracker.test_path = MinitestAdapter.test_path(self)
           Knapsack.tracker.start_timer
         end
@@ -30,27 +29,21 @@ module Knapsack
         ::Minitest::Test.send(:include, BindTimeTrackerMinitestPlugin)
 
         ::Minitest.after_run do
-          #::Kernel.at_exit do
           Knapsack.logger.info(Presenter.global_time)
-          #end
         end
       end
 
 
       def bind_report_generator
         Minitest.after_run do
-          #::Kernel.at_exit do
           Knapsack.report.save
           Knapsack.logger.info(Presenter.report_details)
-          #end
         end
       end
 
       def bind_time_offset_warning
         Minitest.after_run do
-          #::Kernel.at_exit do
           Knapsack.logger.warn(Presenter.time_offset_warning)
-          #end
         end
       end
 
