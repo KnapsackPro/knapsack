d257fc6809537793e1b85ef3405c1e2d2aa7d261
ArturT <arturtrzop@gmail.com>
2015-05-01 14:22:34 +0200

    Add example minitest with rspec syntax

---
+++ test_examples/fast/spec_test.rb
@@ -0,0 +1,25 @@
+require 'minitest/autorun'
+
+class FakeCalculator
+  def add(x, y)
+    x + y
+  end
+
+  def mal(x, y)
+    x * y
+  end
+end
+
+describe FakeCalculator do
+  before do
+    @calc = FakeCalculator.new
+  end
+
+  it '#add' do
+    @calc.add(2, 3).must_equal 5
+  end
+
+  it '#mal' do
+    @calc.mal(2, 3).must_equal 6
+  end
+end
