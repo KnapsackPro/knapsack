6eb21e6a2d6f6ca3614783ebf03e866059f37346
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2015-07-20 09:18:06 +0200

    Add knapsack binary

---
+++ lib/knapsack/runners/rspec_runner.rb
@@ -0,0 +1,23 @@
+module Knapsack
+  module Runners
+    class RSpecRunner
+      def self.run(args)
+        allocator = Knapsack::AllocatorBuilder.new(Knapsack::Adapters::RspecAdapter).allocator
+
+        puts
+        puts 'Report specs:'
+        puts allocator.report_node_tests
+        puts
+        puts 'Leftover specs:'
+        puts allocator.leftover_node_tests
+        puts
+
+        cmd = %Q[bundle exec rspec #{args} --default-path #{allocator.test_dir} -- #{allocator.stringify_node_tests}]
+
+        system(cmd)
+        exit($?.exitstatus)
+
+      end
+    end
+  end
+end
