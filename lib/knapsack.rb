74a0a0edebecbde158a14005d676d4456ba3b4a8
ArturT <arturtrzop@gmail.com>
2015-07-20 14:00:26 +0200

    Add minitest runner. TODO: figure out how to move lib/tasks/knapsack_minitest.rake to runner

--- lib/knapsack.rb
+++ lib/knapsack.rb
@@ -18,6 +18,7 @@ require_relative 'knapsack/adapters/cucumber_adapter'
 require_relative 'knapsack/adapters/minitest_adapter'
 require_relative 'knapsack/runners/rspec_runner'
 require_relative 'knapsack/runners/cucumber_runner'
+require_relative 'knapsack/runners/minitest_runner'
 
 module Knapsack
   class << self
---
+++ lib/knapsack/runners/minitest_runner.rb
@@ -0,0 +1,9 @@
+module Knapsack
+  module Runners
+    class MinitestRunner
+      def self.run(args)
+        # TODO
+      end
+    end
+  end
+end
