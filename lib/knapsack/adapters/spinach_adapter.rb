583966bdd2ffcb5b7e345ff08e43e2fb1d59a8af
ArturT <arturtrzop@gmail.com>
2016-06-03 17:13:19 +0200

    Add :: prefix to Spinach

--- lib/knapsack/adapters/spinach_adapter.rb
+++ lib/knapsack/adapters/spinach_adapter.rb
@@ -1,5 +1,3 @@
-require 'spinach'
-
 module Knapsack
   module Adapters
     class SpinachAdapter < BaseAdapter
@@ -7,29 +5,29 @@ module Knapsack
       REPORT_PATH = 'knapsack_spinach_report.json'
 
       def bind_time_tracker
-        Spinach.hooks.before_scenario do |scenario_data, step_definitions|
+        ::Spinach.hooks.before_scenario do |scenario_data, step_definitions|
           Knapsack.tracker.test_path = SpinachAdapter.test_path(scenario_data)
           Knapsack.tracker.start_timer
         end
 
-        Spinach.hooks.after_scenario do
+        ::Spinach.hooks.after_scenario do
           Knapsack.tracker.stop_timer
         end
 
-        Spinach.hooks.after_run do
+        ::Spinach.hooks.after_run do
           Knapsack.logger.info(Presenter.global_time)
         end
       end
 
       def bind_report_generator
-        Spinach.hooks.after_run do
+        ::Spinach.hooks.after_run do
           Knapsack.report.save
           Knapsack.logger.info(Presenter.report_details)
         end
       end
 
       def bind_time_offset_warning
-        Spinach.hooks.after_run do
+        ::Spinach.hooks.after_run do
           Knapsack.logger.log(
             Presenter.time_offset_log_level,
             Presenter.time_offset_warning
