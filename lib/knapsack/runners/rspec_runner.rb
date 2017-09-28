6c9e7587f874de1fc0a87eff84e48eebffff66ae
Judy Wacker <judy@enjoy.com>
2017-09-28 12:06:38 -0700

    use bin/rspec instead of bundle exec rspec

--- lib/knapsack/runners/rspec_runner.rb
+++ lib/knapsack/runners/rspec_runner.rb
@@ -12,7 +12,7 @@ module Knapsack
         Knapsack.logger.info allocator.leftover_node_tests
         Knapsack.logger.info
 
-        cmd = %Q[bundle exec rspec #{args} --default-path #{allocator.test_dir} -- #{allocator.stringify_node_tests}]
+        cmd = %Q[bin/rspec #{args} --default-path #{allocator.test_dir} -- #{allocator.stringify_node_tests}]
 
         system(cmd)
         exit($?.exitstatus) unless $?.exitstatus == 0
