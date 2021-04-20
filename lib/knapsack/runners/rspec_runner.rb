7c47f46f51bb06f2867dd9e681f3fbaefcd4e302
Alexander Jeurissen <alexander@hackerone.com>
2021-04-20 18:48:00 +0200

    Add puts, so we print out valuable knapsack info (even with log disabled)

--- lib/knapsack/runners/rspec_runner.rb
+++ lib/knapsack/runners/rspec_runner.rb
@@ -5,12 +5,19 @@ module Knapsack
         allocator = Knapsack::AllocatorBuilder.new(Knapsack::Adapters::RSpecAdapter).allocator
 
         Knapsack.logger.info
+        puts ''
         Knapsack.logger.info 'Report specs:'
+        puts 'Report specs:'
         Knapsack.logger.info allocator.report_node_tests
+        puts allocator.report_node_tests
         Knapsack.logger.info
+        puts ''
         Knapsack.logger.info 'Leftover specs:'
+        puts 'Leftover specs:'
         Knapsack.logger.info allocator.leftover_node_tests
+        puts allocator.leftover_node_tests
         Knapsack.logger.info
+        puts
 
         # NOTE: return if there are no specs to execute for this node.
         # This can occurr if test_file_list_source_file is used with less then CI_NODES specs
