768825a5b2cacc58ea579fe11b36ef3ede8a75ed
Madison Sites <madison@enjoy.com>
2021-06-15 12:22:27 -0700

    Output command to run bisect (non-parallel)

--- lib/knapsack/runners/rspec_runner.rb
+++ lib/knapsack/runners/rspec_runner.rb
@@ -12,12 +12,20 @@ module Knapsack
         Knapsack.logger.info allocator.leftover_node_tests
         Knapsack.logger.info
 
-        cmd = %Q[bin/rspec #{args} --default-path #{allocator.test_dir} -- #{allocator.stringify_node_tests}]
+        # https://github.com/rails/spring/issues/113#issuecomment-135896880
+        generated_seed = srand % 0xFFFF unless ARGV.any? { |arg| arg =~ /seed/ }
+
+        circle_cmd = %Q[bin/rspec #{args} --seed #{generated_seed} --default-path #{allocator.test_dir} -- #{allocator.stringify_node_tests}]
+        files = circle_cmd.scan(/"\/home\/circleci\/super_samurai\/.*"/)
+        files_without_circle_path = files.map { |f| f.gsub(Regexp.new(Regexp.escape('/home/circleci/super_samurai/')), '') }.join('')
 
         puts "\n\nunderlying rspec command invoked by knapsack:"
-        puts cmd
+        puts circle_cmd
         puts "\n\n"
-        system(cmd)
+        puts "to run bisect (non-parallel):"
+        puts "DISABLE_SPRING=1 bin/rspec --seed #{generated_seed} #{files_without_circle_path} --bisect"
+        puts "\n\n"
+        system(circle_cmd)
         exit($?.exitstatus) unless $?.exitstatus == 0
       end
     end
